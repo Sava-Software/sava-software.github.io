18b29f0977fb925e5a056e3ec3f77f16b2b15559
Fabio Federici <f@federici.xyz>
2024-09-04 16:47:36 +0200

    more workflow test

--- src/components/SavaWrapper.tsx
+++ src/components/SavaWrapper.tsx
@@ -1,9 +1,10 @@
 import SavaPage from './SavaPage'
+import leaves from '../assets/leaves.png'
 
 export default function Component() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-[100vw]">
-      <img src="../src/assets/leaves.png" alt="leaves" className="absolute top-0 left-0 w-full h-full object-cover opacity-25 blur-sm" />
+      <img src={leaves} alt="leaves" className="absolute top-0 left-0 w-full h-full object-cover opacity-25 blur-sm" />
       <SavaPage />
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sava/website/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/sava/website/",
 })
